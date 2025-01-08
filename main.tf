b990ff7ae14178049d5bc7a31997f0a2fdfdcf1f
dejewski <dejewski@gmail.com>
2025-01-08 11:48:34 +0100

    fix metadata startup script

--- main.tf
+++ main.tf
@@ -67,6 +67,6 @@ resource "google_compute_instance" "blog" {
   }  
 
 
-  metadata_starup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
+  metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
   allow_stopping_for_update = true
 }
