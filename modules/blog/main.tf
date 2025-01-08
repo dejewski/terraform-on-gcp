e87883eddd74fba323cc38d775b3cad078714789
dejewski <dejewski@gmail.com>
2025-01-08 12:42:30 +0100

    removed network

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -30,11 +30,6 @@ module "app_network" {
   ]
 }
 
-resource "google_compute_network" "app" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
 resource "google_compute_subnetwork" "app" {
   name          = var.network_name
   ip_cidr_range = var.network_ip_range
